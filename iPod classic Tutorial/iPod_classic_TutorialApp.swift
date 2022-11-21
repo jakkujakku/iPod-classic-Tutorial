9ca5358c1b3b11b30305c5d1a5c3a20e632b22d2
jakkujakku <jakkujakku98@gmail.com>
2022-11-22 06:19:22 +0900

    Create a iPod classic Tutorial and README.md

---
+++ iPod classic Tutorial/iPod_classic_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPod_classic_TutorialApp.swift
+//  iPod classic Tutorial
+//
+//  Created by (^ㅗ^)7 iMac on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iPod_classic_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
